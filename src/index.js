f4e93224176952f0c62cdfacf704cf97a9ad7401
Anna Bagrowska <anna_bagrowska@wp.pl>
2018-07-02 11:17:32 +0200

    add import in index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import reducer from './reducer.js';
+import {addComment} from './action.js';
 
 const store = createStore(reducer);
 
@@ -13,4 +15,7 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
+
+store.dispatch(addComment('pierwszy komentarz'));
+store.dispatch(addComment('drugi komentarz'));
 
