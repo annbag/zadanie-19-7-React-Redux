50f2b94b290a67ea5ff57b0261a8aff6f6702e5f
Anna Bagrowska <anna_bagrowska@wp.pl>
2018-07-02 11:31:30 +0200

    fixes in comments.js

--- src/comments.js
+++ src/comments.js
@@ -4,18 +4,12 @@ import {REMOVE_COMMENT} from './actions';
 import {THUMB_UP_COMMENT} from './actions';
 import {THUMB_DOWN_COMMENT} from './actions';
 
-
-const initialState = {
-    comments: [],
-    users: []
-};
-
 function comments(state = [], action) {
     switch(action.type) {
         case ADD_COMMENT:
             return [{
                 id: action.id,
-                text: action.text
+                text: action.text,
                 votes: 0
             }
             , ...state];    
@@ -28,20 +22,20 @@ function comments(state = [], action) {
               }              
               return comment;
             });       
-        case THUMB_UP_COMMENT;
+        case THUMB_UP_COMMENT:
           return state.map(comment => {
               if (comment.id === action.id) {
                 comment.votes = comment.votes + 1
               }
               return comment;
-            }
-        case THUMB_DOWN_COMMENT;
+            });
+        case THUMB_DOWN_COMMENT:
           return state.map(comment => {
               if (comment.id === action.id) {
                 comment.votess = comment.votes - 1
               }
               return comment;
-            }
+            });
         default:
             return state;
     }
