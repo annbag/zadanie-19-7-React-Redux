20e0f059a8c40f6b5c2d2de2d0824a23488b384c
Anna Bagrowska <anna_bagrowska@wp.pl>
2018-07-04 10:02:29 +0200

    add thumbDownCommennt in CommentContainer.js

--- src/CommentContainer.js
+++ src/CommentContainer.js
@@ -1,9 +1,11 @@
 import {connect} from 'react-redux';
-import Comment from './Comment';
-import {thumbUpComment} from './index'
+import Comment from './comment.js';
+import {thumbUpComment} from './index.js';
+import {thumbDownComment} from './index.js'
 
 const mapDispatchToProps = dispatch => ({
-  thumbUpComment: (id) => dispatch(thumbUpComment(id))
+  thumbUpComment: (id) => dispatch(thumbUpComment(id)),
+  thumbDownComment: (id) => dispatch(thumbDownComment(id))
 });
 
 export default connect(null, mapDispatchToProps)(Comment);
